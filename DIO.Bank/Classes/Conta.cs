577d461cdcaf455004dd7673b2debcf265939304
vitaoTheEagle <huugo.viictor.bueno@gmail.com>
2021-10-01 00:39:49 -0300

    Adiciona Funções

--- DIO.Bank/Classes/Conta.cs
+++ DIO.Bank/Classes/Conta.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace DIO.Bank
 {
     public class Conta
@@ -16,6 +18,47 @@ namespace DIO.Bank
             this.Saldo = saldo;
             this.Credito = credito;
             this.Nome = nome;
+        }
+
+        public bool Sacar(double valorSaque)
+        {
+            // Validação de saldo suficiente
+            if (this.Saldo - valorSaque < this.Credito * -1)
+            {
+                Console.WriteLine("Saldo Insuficiente");
+                return false;
+            }
+
+            this.Saldo -= valorSaque;
+
+            Console.WriteLine("Saldo atual da conta de {0} é {1}", this.Nome, this.Saldo);
+
+            return true;
+        }
+
+        public void Depositar(double valorDeposito)
+        {
+            this.Saldo += valorDeposito;
+
+            Console.WriteLine("Saldo atual da conta de {0} é {1}", this.Nome, this.Saldo);
+        }
+
+        public void Transferir(double valorTransferencia, Conta contaDestino)
+        {
+            if (this.Sacar(valorTransferencia))
+            {
+                contaDestino.Depositar(valorTransferencia);
+            }
+        }
+
+        public override string ToString()
+        {
+            string retorno = "";
+            retorno += "TipoConta " + this.TipoConta + " | ";
+            retorno += "Nome " + this.Nome + " | ";
+            retorno += "Saldo " + this.Saldo + " | ";
+            retorno += "Credito " + this.Credito;
+            return retorno;
         }
     }
 }
